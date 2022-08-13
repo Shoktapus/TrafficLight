f420afb3c8a12bfd5f8e013540c7889ea704a5da
Shoktapus <sinclair8075@gmail.com>
2022-08-13 21:03:04 +0000

    p

--- src/js/component/light.jsx
+++ src/js/component/light.jsx
@@ -1,18 +1,24 @@
 import React from "react";
-import {useState } from 'react';
+import { useState } from "react";
 //include images into your bundle
-
 
 //create your first component
 const Light = (props) => {
-    let currentClass = "light"
-    if (props.thisColor == props.currentColor) {
-        currentClass = "active"
-    }
-	return (
-		<div className={currentClass} onClick={() => props.setColor(props.thisColor)}>{props.thisColor}</div>
-        
-	);
+  let currentClass = "light";
+  let currentStyles = {backgroundColor:props.thisColor}
+  if (props.thisColor == props.currentColor) {
+    currentClass = "active";
+    currentStyles.boxShadow = "0px 0px 40px 20px " + props.thisColor
+  }
+  return (
+    <div
+      style={ currentStyles}
+      className={currentClass}
+      onClick={() => props.setColor(props.thisColor)}
+    >
+      {props.thisColor}
+    </div>
+  );
 };
 
 export default Light;
